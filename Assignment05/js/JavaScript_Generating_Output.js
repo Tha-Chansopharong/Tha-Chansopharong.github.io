0aa6882349e7c9430022790cad9f7ec365baa157
Thachansopharong <sopharongtha13@gmail.com>
2023-09-30 10:31:06 +0700

    assignment 5

--- Assignment05/js/JavaScript_Generating_Output.js
+++ Assignment05/js/JavaScript_Generating_Output.js
@@ -1,17 +1,4 @@
-document.write("<h1>• Writing into the browser console</h1>");
-document.write("consol.log(variable)");
-
-var firstName = 'Sopharong';
-console.log(firstName);
-
-document.write("<h1>• Writing into an alert dialog box</h1>");
-document.write("window.alert()");
-
-document.write("<h1>• Problem with document.write() method</h1>");
-document.write(" it doesn't support XHTML");
-
-document.write("<h1>• Writing into an HTML element  //innerHTML</h1>")
-document.write("innerHTML");
-
-
-
+var a ="I am a Student";
+console.log(a);
+// alert('Hello world');
+document.getElementById('myid').innerHTML="Hello world";
